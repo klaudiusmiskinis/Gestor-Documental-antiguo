60900e2a6e2b758415a03148079c315a81375f65
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-24 12:26:12 +0200

    Definiendo funcionalidad de atributos

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
+const { throws } = require('assert');
 const { dir } = require('console');
 const fs = require('fs');
 const { get } = require('http');
-const path = require('path')
+const { arch } = require('os');
+const path = require('path');
+const Directorio = require('./clases/directorio');
 
+// Variables
 const rutaRaiz = '//serverdoc/E/z_Informatica/'
-
-let dirUno = []
+const directorios = []
 
 getDir(rutaRaiz)
 
 function getDir(ruta) {
-    fs.readdirSync(ruta).forEach(file => {
-        if(!file.includes('.')){
-            dirUno.push(file)
-            console.log(f)
+    fs.readdirSync(ruta).forEach(dir => {
+        if(!dir.includes('.')){
+            let dirObj = new Directorio(dir, ruta)
+            let archivos = getFiles(rutaRaiz + dir + '/')
+            dirObj.setArchivos(archivos)
+            directorios.push(dirObj)
+            console.log(directorios)
         }
     });
-
-    dirUno.forEach(dir => {
-        if (dir == 'Z_Documentacio') {
-            let a = rutaRaiz + dir +'/'
-            getFiles(a)
-        }
-    })
 }
 
 function getFiles(ruta) {
+    let archivos = []
     fs.readdirSync(ruta).forEach(file => {
         if(file.includes('.')){
-            dirUno.push(file)
-            console.log(f)
+            archivos.push(file)
         }
     });
+    return archivos
 }
---
+++ clases/directorio.js
@@ -0,0 +1,42 @@
+class Directorio {
+    constructor(nombre, ruta, archivos, subDirectorios){
+        this.nombre = nombre;
+        this.ruta = ruta;
+        this.archivos = archivos;
+        this.subDirectorios = subDirectorios;
+    }
+
+    getNombre() {
+        return this.nombre;
+    }
+
+    getRuta() {
+        return this.ruta;
+    }
+
+    getArchivos() {
+        return this.archivos;
+    }
+
+    getSubDirectiorios() {
+        return this.subDirectorios;
+    }
+
+    setNombre(nombre) {
+        this.nombre = nombre;
+    }
+
+    setRuta(ruta) {
+        this.ruta = ruta;
+    }
+
+    setArchivos(archivos){
+        this.archivos = archivos;
+    }
+
+    setSubDirectorios(subDirectorios) {
+        this.subDirectorios = subDirectorios
+    }
+}
+
+module.exports = Directorio
