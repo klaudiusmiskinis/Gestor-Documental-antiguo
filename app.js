2a93f64d7e9983298751bd5d58783b3f986361fe
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-24 12:38:23 +0200

    Subdir de objeto funcional

--- app.js
+++ app.js
@@ -15,13 +15,25 @@ getDir(rutaRaiz)
 function getDir(ruta) {
     fs.readdirSync(ruta).forEach(dir => {
         if(!dir.includes('.')){
-            let dirObj = new Directorio(dir, ruta)
+            let dirObj = new Directorio(dir, (ruta + dir))
             let archivos = getFiles(rutaRaiz + dir + '/')
+            let subDir = getSubDir(rutaRaiz + dir + '/')            
+            dirObj.setSubDirectorios(subDir)
             dirObj.setArchivos(archivos)
             directorios.push(dirObj)
             console.log(directorios)
         }
     });
+}
+
+function getSubDir(ruta) {
+    let subDirectorios = []
+    fs.readdirSync(ruta).forEach(subDir => {
+        if(!subDir.includes('.')){
+            subDirectorios.push(subDir)
+        }
+    });
+    return subDirectorios
 }
 
 function getFiles(ruta) {
