631b303da79331245387e05087244b301b7d7206
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-24 11:16:07 +0200

    Añadiendo funcionalidades y leyendo dir/files

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-const fs = require('fs')
+const { dir } = require('console');
+const fs = require('fs');
+const { get } = require('http');
 const path = require('path')
 
-fs.readdirSync(__dirname+'/try/').forEach(file => {
-    console.log(file);
-});
+const rutaRaiz = '//serverdoc/E/z_Informatica/'
+
+let dirUno = []
+
+getDir(rutaRaiz)
+
+function getDir(ruta) {
+    fs.readdirSync(ruta).forEach(file => {
+        if(!file.includes('.')){
+            dirUno.push(file)
+            console.log(f)
+        }
+    });
+
+    dirUno.forEach(dir => {
+        if (dir == 'Z_Documentacio') {
+            let a = rutaRaiz + dir +'/'
+            getFiles(a)
+        }
+    })
+}
+
+function getFiles(ruta) {
+    fs.readdirSync(ruta).forEach(file => {
+        if(file.includes('.')){
+            dirUno.push(file)
+            console.log(f)
+        }
+    });
+}
