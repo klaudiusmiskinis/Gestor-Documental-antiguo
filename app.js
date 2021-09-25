fc71324c31a582a238c33e0af69cf1a2135a4ac0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-25 18:58:27 +0200

    Creando las rutas a partir de la funcion inicializar

--- app.js
+++ app.js
@@ -5,29 +5,18 @@ const { arch } = require('os');
 const express = require('express')
 const path = require('path');
 const fs = require('fs');
+const router = express.Router();
 const app = express()
 const Directorio = require('./clases/directorio');
 
 // Variables
 const rutaRaiz = 'C:/Users/Klau/AppData/Local/Programs/Microsoft VS Code/'
+let creado = false;
 const estructura = []
 const subDirectorios = []
+
 try {
     inicializar(rutaRaiz)
-    console.log('Entrando a rutaraiz')
-    estructura.forEach(dir => {
-        app.get('/' + dir.getNombre(), (req, res) => {
-            console.log('Conectando' + app)
-            res.render('index.ejs', { dir: dir = {
-                nombre: dir.getNombre(),
-                ruta: dir.getRuta(),
-                archivos: dir.getArchivos(),
-                subDirectorios: dir.getInnerDir()
-            },
-            estructura: estructura
-        })
-        })
-    })
 } catch (e) {
     console.log(e)
 }
@@ -40,7 +29,6 @@ function inicializar(ruta) {
     fs.readdirSync(ruta).forEach(dir => {
     let type = fs.lstatSync(ruta + dir)
         if(type.isDirectory()) {
-            console.log(ruta + dir)
             let dirObj = new Directorio(dir, (ruta + dir + '/'))
             dirObj.setArchivos(dirObj.getFiles())
             dirObj.setSubDirectorios(dirObj.getInnerDir())
@@ -48,12 +36,16 @@ function inicializar(ruta) {
             estructura.push(dirObj)
         }
     });
+    estructura.forEach(dir => {
+        app.get('/' + dir.getNombre(), (req, res) => {
+            res.render('index.ejs', { estructura: estructura })
+        })
+    })
 }
 
 // HTTP METHODs
 app.get('/', (req, res) => {
-    console.log(subDirectorios)
-    res.redirect('/' + estructura[3].getNombre())
-})
+    res.redirect('/'+estructura[0].getNombre())
+});
 
 app.listen(3000)
