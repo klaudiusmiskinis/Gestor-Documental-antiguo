c049b62c043eb654bf2939cb4e5f13473a267263
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-27 13:22:20 +0200

    Cambiando a modulo que facilita el escaneo

--- app.js
+++ app.js
@@ -2,56 +2,25 @@ const { throws } = require('assert');
 const { dir } = require('console');
 const { get } = require('http');
 const { arch } = require('os');
-const express = require('express')
 const path = require('path');
-const fs = require('fs');
+const express = require('express')
+var wrench = require("wrench");
 const app = express()
+const fs = require('fs');
 const Directorio = require('./clases/directorio');
 
 // Variables
 const rutaRaiz = '//Serverdoc/e/z_Informatica/Z_Documentacio/'
-const nivelU = []
-const nivelD = []
-const nivelT = []
-const nivelC = []
+var files = wrench.readdirSyncRecursive(rutaRaiz);
+    wrench.readdirRecursive(rutaRaiz, function (error, files) {
+});
 
 // Config
 app.set('view engine', 'ejs')
 
+//HTTPs
 app.get('/', (req, res) => {
-    try {
-        inicializar(rutaRaiz)
-        estructura.forEach(dir => {
-            app.get('/' + dir.getNombre(), (req, res) => {
-                res.render('index.ejs', { estructura: estructura[1].getNombre() })
-            })
-        })
-    } catch (e) {
-        console.log('Error en la inicialización.')
-    }
-    res.render('index.ejs', { estructura: estructura[1].getNombre() })
+    console.log(files)
 })
 
-
-
-
 app.listen(3000)
-
-async function inicializar(ruta) {
-    await fs.readdirSync(ruta).forEach(dir => {
-        if(!dir.includes('.')){
-            let dirObj = new Directorio(dir, (ruta + dir + '/'), '/')
-            dirObj.setArchivos(dirObj.getFiles())
-            dirObj.setSubDirectorios(dirObj.getInnerDir())
-            estructura.push(dirObj)
-        }
-    });
-    console.log('EST: ' + estructura)
-    estructura.forEach(dir => {
-        console.log('dir ' + dir.getPadre())
-        directorios.push(dir.getSubDirectorios())
-    })
-    directorios.forEach(dir => {
-        directorios.push(dir.getPadre())
-    })
-}
