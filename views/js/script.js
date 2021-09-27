54eb4418f078fb52aca0e672b49ce81c2fcfc2e8
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-27 22:18:19 +0200

    Eliminando carpetas inutilizadas

--- views/js/script.js
+++
@@ -1,9 +0,0 @@
-let estructura = []
-
-getEstructura()
-
-async function getEstructura() {
-    let response = await fetch('/estructura');
-    estructura = await response.text()
-    console.log(estructura)
-}
