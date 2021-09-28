565cee71bce0802333882351b59a0e0308306a9d
Klau <73123094+klaudiusmiskinis@users.noreply.github.com>
2021-09-28 14:01:15 +0200

    Cambios significativos en el codigo
    - Se hace el escaneo recursivo perfectamente.
    - Se asignan los diferentes archivos a su correspondiente carpeta.
    - Se sabe quien es el padre de cada carpeta.
    - Se guardan todas las carpetas con sus respectivos archivos en un Array.
    - Todos estos directorios se tratan como objetos.
    - Los datos se validan correctamente.
    
    La carpeta root debe configurarse para un mejor despliegue automático.

--- app.js
+++ app.js
@@ -2,38 +2,109 @@ const { throws } = require('assert');
 const { dir } = require('console');
 const { get } = require('http');
 const { arch, type } = require('os');
-const path = require('path');
 const express = require('express')
 var wrench = require("wrench");
 const app = express()
 const fs = require('fs');
-const Directorio = require('./clases/directorio');
+const { sep } = require('path');
 
 // Variables
-const rutaRaiz = 'C:/xampp/htdocs/'
+const rutaRaiz = '//serverdoc/e/X_Dpt_RRHH i Qualitat/SGC y seguridad alimentaria/Sistema documental/'
 const allDirectories = []
 const allFiles = []
 var files = wrench.readdirSyncRecursive(rutaRaiz);
-    wrench.readdirRecursive(rutaRaiz, function (error, files) {});
+wrench.readdirRecursive(rutaRaiz, function (error, files) {});
 
+// Funciones
+// GET directorios y archivos recursivos con replace de \ a /
 files.forEach(file => {
+    file = file.replace(/\\/g, '/');
     if(fs.lstatSync(rutaRaiz + file).isDirectory()) {
         allDirectories.push(file)
     } else  if(fs.lstatSync(rutaRaiz + file).isFile()){
         allFiles.push(file)
-    } else {
-        console.log('Ni archivo ni directorio :O')
     }
 })
 
+let directorio;
+let dirFilter = []
 
+//LOOP ALLDIR
+function actualizar() {
+    dirFilter.push(
+        dirObj = {
+            nombre: '/',
+            padre: rutaRaiz,
+            rutaRelativa: rutaRaiz,
+            rutaAbsoluta: rutaRaiz,
+            archivos: []
+        }
+    )
+    
+    allDirectories.forEach(dir => {
+        let separador = dir.split('/')
+        directorio = getPadre(separador)
+        function getPadre(sep) {
+            if (sep.length > 1) {
+                let dirObj = {
+                    nombre: sep[sep.length -1],
+                    padre: sep[sep.length -2] + '/',
+                    rutaRelativa: dir + '/',
+                    rutaAbsoluta: rutaRaiz + dir + '/',
+                    archivos: []
+                }
+                return dirObj;  
+            } else {
+                let dirObj = {
+                    nombre: sep[sep.length -1],
+                    padre: '/',
+                    rutaRelativa: '/',
+                    rutaAbsoluta: rutaRaiz,
+                    archivos: []
+                } 
+                return dirObj; 
+            }
+        }    
+        dirFilter.push(directorio)
+    })
+    
+    //LOOP ALLFILES
+    allFiles.forEach(file => {
+        let separador = file.split('/')
+        dirFilter.find(dir => {
+            if (dir.nombre == separador[separador.length-2] && dir.padre == ((separador[separador.length-3]) + '/'))  {
+                dir.archivos.push(separador[separador.length-1])
+            } else if (dir.nombre == separador){
+                dir.archivos.push(separador)
+            }
+        })
+    })
+
+    dirFilter.forEach(dir => {
+        if (dir.nombre == '/') {
+            app.get('/home', (req, res) => {
+                res.render('index.ejs', {dir: dir})
+            })
+        } else {
+            app.get(dir.padre + '/' + dir.nombre, (req, res) => {
+                res.render('index.ejs')
+            })
+        }
+    })
+    console.log(app._router.stack)
+}
 // Config
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/views'))
 
 //HTTPs
 app.get('/', (req, res) => {
-    console.log(files)
+    actualizar();
+    res.redirect('/home')
+})
+
+app.get('/home', (req, res) => {
+    res.render('index.ejs', {dir: dir})
 })
 
 app.listen(3000)
