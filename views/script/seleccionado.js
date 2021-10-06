f135ff2885d92e29f0ab00cdae79ae8db491afca
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-06 12:13:42 +0200

    Manejando el error y el funcionamiento del boton

--- views/script/seleccionado.js
+++ views/script/seleccionado.js
@@ -1,3 +1,10 @@
+if(document.getElementById('volver')) {
+    document.getElementById('volver').onclick = function() {
+        volver()
+    }
+}
+
+//FUNCTION
 function volver() {
     window.history.back()
     console.log(history)
