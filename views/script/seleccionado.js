183bde26ff96be9e46e0c475137ae2da72129e81
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-09 16:47:22 +0200

    Añadiendo funciones de popUp de SweetAlert

--- views/script/seleccionado.js
+++ views/script/seleccionado.js
@@ -15,3 +15,31 @@ function volver() {
     window.history.back()
     console.log(history)
 }
+
+window.onload = function() {
+    let elementos = document.getElementsByClassName('cerrar')
+    for(let i = 0; i < elementos.length; i++) {
+        elementos[i].addEventListener('click', eliminar())
+    }
+}
+
+
+function eliminar() {
+    Swal.fire({
+        title: '¿Estás seguro de que quieres eliminar este archivo?',
+        text: "Una vez eliminado, no se podrá recuperar.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar.'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            '¡Eliminado!',
+            'El archivo ha sido eliminado.',
+            'success'
+          )
+        }
+      })
+}
