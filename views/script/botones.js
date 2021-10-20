dd5edebbd4d29b110f0158f06d53212784dfff66
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-20 12:57:34 +0200

    Añadiendo funciones de boton a botones.js

--- views/script/botones.js
+++ views/script/botones.js
@@ -62,3 +62,30 @@ $("#subir-boton").on("click", function(e) {
         }
     }
 })
+
+function eliminar(e){
+    e.preventDefault();
+    var queryString = $('#' + this.id).serialize();
+    let archivoNom = queryString.split('=')[1]
+    archivoNom = decodeURI(archivoNom)
+    Swal.fire({
+        title: '¿Estás seguro de querer eliminar el archivo ' + archivoNom + '?',
+        text: "No se podrá recuperar.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: 'var(--col)',
+        cancelButtonColor: 'var(--col)',
+        confirmButtonText: 'Si, eliminar.',
+        cancelButtonText: 'Cancelar.',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutDown'
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+            eliminarArchivo(this.id);
+        }
+      })
+};
