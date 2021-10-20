77ed6b202b082467f354f9d7357c962f6d7ff9ab
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-20 09:49:28 +0200

    Subir con versiones y sobreescribir ya funciona

--- views/script/seleccionado.js
+++ views/script/seleccionado.js
@@ -17,33 +17,53 @@ window.onDOMContentLoaded = function(){
     }, 1000)
 };
 
-$("#subir-input").on("click", function(e) {
+$("#subir-boton").on("click", function(e) {
     e.preventDefault();
-    Swal.fire({
-        title: 'Ya hay una archivo con ese nombre<br>' +
-                '¿Quieres crear una nueva versión?',
-        text: "Se creará una nueva versión del archivo",
-        icon: 'warning',
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonColor: 'var(--col)',
-        cancelButtonColor: 'var(--col)',
-        confirmButtonText: 'Crear una nueva versión.',
-        denyButtonText: 'No, sobreescribir el archivo',
-        cancelButtonText: 'Cancelar.',
-        showClass: {
-            popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-            popup: 'animate__animated animate__fadeOutDown'
+    let encontrado = null;
+    var archivo = document.getElementById('subir-campo');
+    let nombre = archivo.files[0].name;
+    let archivos = document.getElementsByClassName('archivo');
+    if (archivos.length == 0 && nombre.length > 0) {
+        $('#subir').submit();
+    }
+    for(let i = 0; i < archivos.length; i++) {
+        if(archivos[i].innerHTML === nombre){
+            encontrado = true;
+            Swal.fire({
+                title: 'Ya hay una archivo con ese nombre<br>' +
+                        '¿Quieres crear una nueva versión?',
+                text: "Se creará una nueva versión del archivo",
+                icon: 'warning',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonColor: 'var(--col)',
+                cancelButtonColor: 'var(--col)',
+                confirmButtonText: 'Crear una nueva versión.',
+                denyButtonText: 'No, sobreescribir el archivo',
+                cancelButtonText: 'Cancelar.',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__fadeOutDown'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('subir').action += '?nuevaversion=true';
+                    $('#subir').submit();
+                } else if (result.isDenied) {
+                    document.getElementById('subir').action += '?nuevaversion=false';
+                    $('#subir').submit();
+                } 
+            })    
         }
-    }).then((result) => {
-        if (result.isConfirmed) {
-           //SE CONFIRMA -> CREAR UNA NUEVA VERSIÓN DEL ARCHIVO
-        } else if (result.isDenied) {
-           //SE DENIEGA -> SE SOBREESCRIBE EL VIEJO ARCHIVO CON EL NUEVO
+        if (i == (archivos.length -1) && encontrado == null) {
+            encontrado = false;
         }
-    })
+    }
+    if (encontrado == false){
+        $('#subir').submit();
+    }
 })
 
 
