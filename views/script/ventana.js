d07b5f4eacf57446aaa7c5da992c7e5d762ee27d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-20 12:58:51 +0200

    Editando func para ejecutar antes de cargar el DOM

--- views/script/ventana.js
+++ views/script/ventana.js
@@ -1,21 +1,20 @@
-window.onDOMContentLoaded = function(){
-    if (document.title == 'Home') {
-        // $(document.getElementById('container-water')).slideDown();
-    };
-    
+console.log(document.cookie)
+
+document.onreadystatechange = function(e) {
+    if (document.readyState === 'complete') {
+        $(document.body).hide('fast');
+    }
+};
+
+window.onload = function(e) {
+    $(document.body).fadeIn();
     let elementos = document.getElementsByClassName('cerrar');
     for (var i = 0; i < elementos.length; i++) {
         elementos[i].addEventListener('click', eliminar, false);
         elementos[i].id = 'cerrar' + i;
         elementos[i].name = 'formEliminar' + i;
     };
-    setTimeout(function() {
-        if (document.title == 'Home') {
-            // $(document.getElementById('container-water')).slideUp();
-        }
-    }, 1000);
 };
-
 
 if(document.getElementById('volver')){
     document.getElementById('volver').onclick = function(){
@@ -27,6 +26,15 @@ if(document.getElementById('regresar')){
     document.getElementById('regresar').onclick = function(){
         volver();
     };
+};
+
+//FUNCTION
+function volver(){
+    window.history.back();
+};
+
+function eliminarArchivo(id){
+    $('#' + id).submit();
 };
 
 
