8353b8067d6aed39cd993232f3497a42e74104c0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-02 09:51:34 +0100

    Creando middleware para eliminar lineas de codigo innecesarias del app,js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 require('dotenv').config(); 
+const recursivo = require('./middleware/recursivo.js');
 const fileupload = require('express-fileupload');
 const methodOverride = require('method-override');
 const express = require('express');
-const _ = require('underscore');
-const wrench = require('wrench');
 const fs = require('fs');
 const app = express();
 
-
 // Variables
-const rutaRaiz = process.env.RUTA_LOCAL; 
+const rutaRaiz = process.env.RUTA_LOCAL;
 let allDirectories = [];
 let allFiles = [];
 let dirFilter = [];
@@ -17,148 +15,33 @@ let nomRutas = [];
 
 actualizar();
 
-// Funciones
-function getFolder(nom) {
-    let data = {
-        nom: nom,
-        hijos: [],
-        archivos: [],
-        extensiones: [],
-        cantidadArchivos: allFiles.length,
-        cantidadCarpetas: dirFilter.length
-    };
-
-    dirFilter.find(dir => {
-        if (dir.padre == nom) {
-            data.hijos.push(dir);
-        } else if ((nom + '/' + dir.nombre) == dir.rutaRelativa) {
-            data.hijos.push(dir);
-        }
-
-        if (dir.nombre == '/') {
-            data.archivos = dir.archivos;
-        } else if(dir.rutaRelativa == nom) { 
-            data.archivos = dir.archivos;
-            data.archivos = _.without(data.archivos, 'Thumbs.db')
-        } 
-    });
-
-    data.archivos.forEach(archivo => {
-        let extension = archivo.split('.')
-        extension = extension[extension.length - 1]
-        data.extensiones.push(extension)
-    })
-    return data;
-};
-
-function rutas(dir) {
-    if(dir.padre == rutaRaiz) {
-        return dir.nombre
-    } else {
-        return dir.rutaRelativa
-    }
-};
-
 function actualizar() {
-    allDirectories = []
-    allFiles = []
-    dirFilter = []
-    nomRutas = []
-
-    var files = wrench.readdirSyncRecursive(rutaRaiz);
-    wrench.readdirRecursive(rutaRaiz, function (error, files) {});
-
-    files.forEach(file => {
-        file = file.replace(/\\/g, '/');
-        if(fs.lstatSync(rutaRaiz + file).isDirectory()) {
-            allDirectories.push(file)
-        } else if(fs.lstatSync(rutaRaiz + file).isFile()) {
-            allFiles.push(file)
-        }
-    });
-
-    dirFilter.push(
-        dirObj = {
-            nombre: '/',
-            padre: rutaRaiz,
-            rutaRelativa: null,
-            rutaAbsoluta: rutaRaiz,
-            archivos: []
-        }
-    )
-    
-    allDirectories.forEach(dir => {
-        let separador = dir.split('/')
-        let directorio = getPadre(separador)
-        function getPadre(sep) {
-            if (sep.length > 1) {
-                let dirObj = {
-                    nombre: sep[sep.length -1],
-                    padre: sep[sep.length -2] + '/',
-                    rutaRelativa: dir,
-                    rutaAbsoluta: rutaRaiz + dir + '/',
-                    archivos: []
-                }
-                return dirObj;  
-            } else {
-                let dirObj = {
-                    nombre: sep[sep.length -1],
-                    padre: '/',
-                    rutaRelativa: dir,
-                    rutaAbsoluta: rutaRaiz + dir,
-                    archivos: []
-                } 
-                return dirObj; 
-            }
-        }    
-        dirFilter.push(directorio)
-    })
-
-    // LOOP de nombre de rutas para el HTTP
-    dirFilter.forEach(dir => {
-        nomRutas.push(rutas(dir))
-    })
-    
-    // LOOP para saber que archivo pertenece a que directorio
-    allFiles.forEach(file => {   
-        let separador = file.split('/')
-        dirFilter.find(dir => {
-
-            if(separador.length == 1 && dir.nombre == '/') {
-                dir.archivos.push(separador[0])
-            } else if(separador.length == 2 && dir.nombre == separador[0] && dir.padre == '/') {
-                dir.archivos.push(separador[1])
-            }
-
-            if (dir.nombre == separador[separador.length-2] && dir.padre == ((separador[separador.length-3]) + '/'))  {
-                dir.archivos.push(separador[separador.length-1])
-            } else if (dir.nombre == separador) {
-                dir.archivos.push(separador)
-            }
-
-        })
-    })
-
-    let transformado = '';
-    nomRutas.forEach(nom => {
-        transformado = encodeURI(nom)
-        if(nom.charAt(0) == '/') {
-            app.get(transformado, (req, res) => {
-                actualizar();
-                let data = getFolder(nom)
-                res.cookie('position', nom);
-                res.render('index.ejs', {data: data});
-            })
-        } else {
-            app.get('/' + transformado, (req, res) =>{
-                actualizar();
-                let data = getFolder(nom)
-                res.cookie('position', nom);
-                res.render('index.ejs', {data: data});
-            })
-        }
-    })
+    let data = recursivo.recursivo(rutaRaiz);
+    allDirectories = data[0];
+    allFiles = data[1];
+    dirFilter = data[2];
+    nomRutas = data[3];
 }
+
+
+nomRutas.forEach(nom => {
+    transformado = encodeURI(nom)
+    if(nom.charAt(0) == '/') {
+        app.get(transformado, (req, res) => {
+            actualizar();
+            let data = recursivo.getFolder(nom, allFiles, dirFilter)
+            res.cookie('position', nom);
+            res.render('index.ejs', {data: data});
+        })
+    } else {
+        app.get('/' + transformado, (req, res) =>{
+            actualizar();
+            let data = recursivo.getFolder(nom, allFiles, dirFilter)
+            res.cookie('position', nom);
+            res.render('index.ejs', {data: data});
+        })
+    }
+})
 
 // CONFIG
 app.set('view engine', 'ejs')
@@ -177,7 +60,7 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
     actualizar();
     res.cookie('position', '/');
-    res.render('index.ejs', {data: getFolder('/')})
+    res.render('index.ejs', {data: recursivo.getFolder('/', allFiles, dirFilter)})
 })
 
 // POST
