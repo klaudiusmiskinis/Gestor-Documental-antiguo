1e3b5e57a8a7a2356f5614a19d137dde45e591ed
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-03 11:11:39 +0100

    Eliminando textos y añadiendo objetos que contienen los textos.

--- views/script/botones.js
+++ views/script/botones.js
@@ -73,37 +73,8 @@ $('#subir-boton').on('click', function(e) {
 
 $('#info').on('click', function() {
     Swal.fire({
-        title: 'Información sobre los archivos.',
-        html: `
-        Es importante que los archivo que queramos subir no contengan mas que un solo guión bajo.
-        
-        <div class="container mt-2">
-            <div class="row">
-                <div class="col border border-success">
-                    <p class="text-success  mt-2">BIEN ✓</p>
-                </div>
-                <div class="col border border-success">
-                    <p class="fst-italic  mt-2">archivo.txt</p>
-                </div>
-                <div class="col border border-success">
-                    <p class="fst-italic mt-2">archivo_1.txt</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="container mt-1">
-            <div class="row">
-                <div class="col border border-danger">
-                    <p class="text-danger mt-2">MAL ✗</p>
-                </div>
-                <div class="col border border-danger">
-                <p class="fst-italic mt-2">archivo_a_1.txt</p>
-                </div>
-                <div class="col border border-danger">
-                <p class="fst-italic mt-2">a_archivo_1.txt</p>
-                </div>
-            </div>
-        </div>`,
+        title: info.titulo,
+        html: info.html,
         focusConfirm: false,
         showClass: {
           popup: 'animate__animated animate__fadeInDown'
@@ -117,8 +88,8 @@ $('#info').on('click', function() {
 $('.cerrar').on('click', function(e) {
     e.preventDefault();
     Swal.fire({
-        title: '¿Estás seguro de querer eliminar el archivo ' + this.id + ' ?',
-        text: 'No se podrá recuperar.',
+        title: cerrar.titulo,
+        text: cerrar.texto,
         icon: 'warning',
         showCancelButton: true,
         focusConfirm: false,
@@ -146,29 +117,12 @@ function alertaSubir(nombre, version, tipo) {
     } else {
         inputVersion = '';
     }
-    let html = `<div class="form-group">
-                    <label>Datos</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="nombre" placeholder="Nombre*" autocomplete="off">
-                        <input type="text" class="form-control" id="apellidos" placeholder="Apellidos*" autocomplete="off">
-                    </div>
-                    <div class="input-group mt-1">
-                        <input type="text" class="form-control" id="email" placeholder="Nombre del email*" autocomplete="off">
-                        <div class="input-group-append">
-                            <span class="input-group-text" id="arroba">@fruitsponent.com</span>
-                        </div>
-                    </div>
-                    <textarea type="text"  class="mt-1 form-control"  id="motivos" rows="3" placeholder="Motivos*" autocomplete="off"></textarea>
-                    <input type="date" class="mt-1 form-control" id="fecha" autocomplete="off">
-                </div>
-                <div class="form-group mb-3">
-                    <label>Archivo</label>
-                    <input type="text" class="mt-1 form-control" id="arcNombre" placeholder='` + nombre + `' readonly>
-                    ` + inputVersion + `
-                </div>`
     Swal.fire({
-        title: 'Registro de cambios',
-        html: html,
+        title: subir.titulo,
+        html: subir.html + `<div class="form-group mb-3">
+                                <label>Archivo</label>
+                                <input type="text" class="mt-1 form-control" id="arcNombre" placeholder='` + nombre + `' readonly>` + inputVersion + `
+                            </div>`,
         confirmButtonText: 'Crear registro',
         showCancelButton: true,
         cancelButtonText: 'Cancelar',
@@ -231,24 +185,9 @@ function alertaSubir(nombre, version, tipo) {
 }
 
 function alertaEliminar(id) {
-    let html = `<div class="form-group">
-                    <label>Datos</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="nombre" placeholder="Nombre*" autocomplete="off">
-                        <input type="text" class="form-control" id="apellidos" placeholder="Apellidos*" autocomplete="off">
-                    </div>
-                    <div class="input-group mt-1">
-                        <input type="text" class="form-control" id="email" placeholder="Nombre del email*" autocomplete="off">
-                        <div class="input-group-append">
-                            <span class="input-group-text" id="arroba">@fruitsponent.com</span>
-                        </div>
-                    </div>
-                    <textarea type="text"  class="mt-1 form-control"  id="motivos" rows="3" placeholder="Motivos*" autocomplete="off"></textarea>
-                    <input type="date" class="mt-1 form-control" id="fecha" autocomplete="off">
-                </div>`;
     Swal.fire({
-        title: 'Registro de cambios',
-        html: html,
+        title: eliminar.titulo,
+        html: eliminar.html,
         confirmButtonText: 'Crear registro',
         showCancelButton: true,
         cancelButtonText: 'Cancelar',
@@ -285,10 +224,8 @@ function alertaEliminar(id) {
                 url: '/accion',
                 accion: datos
             };
-            console.log(id)
-            console.log(data)
             $.post(data.url, data.accion);
             document.getElementById(id).submit();
-        }
-    })
-}
+        };
+    });
+};
