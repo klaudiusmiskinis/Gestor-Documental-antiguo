9649bd2e9c87a53151181d2313ab20e7486555cc
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-06 17:25:16 +0100

    Creando archivo imagen.js y funcion imageExiste para poder remplazar los imagenes de los archivos que no tienen una imagen para su extensión.

---
+++ views/script/imagen.js
@@ -0,0 +1,13 @@
+let elementos = document.querySelectorAll('.img-archivo')
+for (let i = 0; i < elementos.length; i++) {
+    imageExiste(elementos[i]);
+}
+
+function imageExiste(elemento){
+    let ruta = elemento.currentSrc;
+    $.get(ruta)
+    .fail(function() { 
+        elemento.src = '/assets/img-svg/file.svg';
+        console.clear();
+    })
+}
