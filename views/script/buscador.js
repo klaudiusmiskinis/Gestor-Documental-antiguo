d931c39dedfe6bce1db2d4ff236d07972887b5f2
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-21 13:23:40 +0100

    Pruebas para esconder elementos

--- views/script/buscador.js
+++ views/script/buscador.js
@@ -1,6 +1,8 @@
 $('#buscador').on('input', () => {
     $('.archivo').each(function() {
         if ($(this).html().includes($('#buscador').val())) {
+            let id = '#' + $(this).html();
+            console.log(document.querySelector(id).id);
             $($(this).html()).hide();
         } else {
             $(this).show()
