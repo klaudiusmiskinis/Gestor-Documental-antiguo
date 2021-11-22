889e7b1267704cc906d973f84feedec750b8ada2
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-22 13:25:23 +0100

    Creando eventListener para los buscadores en caso de input

--- views/script/buscador.js
+++ views/script/buscador.js
@@ -1,11 +1,23 @@
-$('#buscador').on('input', () => {
+$('#buscadorFile').on('input', () => {
     $('.archivo').each(function() {
-        if ($(this).html().includes($('#buscador').val())) {
+        if ($(this).html().includes($('#buscadorFile').val())) {
             let padre = $(this).parents()[3];
-            padre.style.display = 'block';
+            padre.style.setProperty('display', 'block');
         } else {
             let padre = $(this).parents()[3];
-            padre.style.display = 'none';
+            padre.style.setProperty('display', 'none');
+        }
+    });
+})
+
+$('#buscadorDir').on('input', () => {
+    $('.directorios').each(function() {
+        if ($(this).html().trim().includes($('#buscadorDir').val())) {
+            let padre = $(this).parents()[2];
+            padre.style.setProperty('display', 'block');
+        } else {
+            let padre = $(this).parents()[2];
+            padre.style.setProperty('display', 'none');
         }
     });
 })
