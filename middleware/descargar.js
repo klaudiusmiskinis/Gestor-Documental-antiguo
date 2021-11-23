15b07aeef85d286b6f065e52381b236c77ef5cfa
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-23 13:38:21 +0100

    Cambiando funcionamiento de get de la cookie posicion

--- middleware/descargar.js
+++ middleware/descargar.js
@@ -1,15 +1,21 @@
 function run(rutaRaiz, req, res) {
-    let position = decodeURI(req.headers.cookie);
-    position = position.split('=')[1];
-    if(position == '%2F') {
-        rutaArchivo = rutaRaiz + req.body.descargar;
-    } else {
-        let archivo = (req.body.descargar).split('%2F').join('/');
-        position = position.split('%2F').join('/');
-        rutaArchivo = rutaRaiz + position + '/' + archivo;
-    }
-    console.log('Descargando:', req.body.descargar);
-}
+    let cookies = req.headers.cookie;
+    cookies = cookies.split(' ')
+    cookies.forEach(cookie => {
+        if (cookie.includes('position')) {
+            let position = cookie.split('=')[1];
+            if(position == '%2F') {
+                rutaArchivo = rutaRaiz + req.body.descargar;
+            } else {
+                let archivo = (req.body.descargar).split('%2F').join('/');
+                position = position.split('%2F').join('/');
+                position = position.split('%20').join(' ');
+                rutaArchivo = rutaRaiz + position + '/' + archivo;
+            }
+            console.log('Descargando:', req.body.descargar);
+        }
+    })
+};
 
 module.exports = {
     run
