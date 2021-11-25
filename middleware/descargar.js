771c4e08e94dc38fd2d954ec4891f6e963b1fa92
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-25 09:52:47 +0100

    Cambiando funcion de listar cookies

--- middleware/descargar.js
+++ middleware/descargar.js
@@ -1,21 +1,18 @@
+const cookies = require('../middleware/cookie');
+
 function run(rutaRaiz, req, res) {
-    let cookies = req.headers.cookie;
-    cookies = cookies.split(' ')
-    cookies.forEach(cookie => {
-        if (cookie.includes('position')) {
-            let position = cookie.split('=')[1];
-            if(position == '%2F') {
-                rutaArchivo = rutaRaiz + req.body.descargar;
-            } else {
-                let archivo = (req.body.descargar).split('%2F').join('/');
-                position = decodeURI(position)
-                position = position.split('%2F').join('/');
-                rutaArchivo = rutaRaiz + position + '/' + archivo;
-            }
-            console.log('Descargando:', req.body.descargar);
-        }
-    })
-};
+    let lista = cookies.listarCookies(req.headers.cookie);
+    let position = lista.position;
+    if(position == '%2F') {
+        rutaArchivo = rutaRaiz + req.body.descargar;
+    } else {
+        let archivo = (req.body.descargar).split('%2F').join('/');
+        position = decodeURI(position)
+        position = position.split('%2F').join('/');
+        rutaArchivo = rutaRaiz + position + '/' + archivo;
+    }
+    console.log('Descargando:', req.body.descargar);
+}
 
 module.exports = {
     run
