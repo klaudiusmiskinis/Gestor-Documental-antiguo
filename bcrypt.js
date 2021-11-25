086591031834474f481fda4bffc5a7d71c8152d9
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-25 23:34:54 +0100

    Pruebas

--- bcrypt.js
+++ bcrypt.js
@@ -2,4 +2,4 @@ const bcrypt = require('bcrypt');
 require('dotenv').config(); 
 const encriptar = bcrypt.hashSync('1234', 12);
 const comparar = bcrypt.compareSync('1234', process.env.PASSWORD_LOGIN);
-console.log(comparar, encriptar);
+console.log(encriptar, comparar);
