0a2cfab5e0ba05f31da9b87f7a1319c11d52aedf
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-28 18:36:56 +0100

    Cambiando manera en la que se exporta la funcion

--- middleware/rol.js
+++ middleware/rol.js
@@ -1,16 +1,14 @@
-function checkRol(req) {
-    let rol;
-    if (!req.session.user){
-        req.session.user = 'none';
-        rol = 'lectura';
-    } else if (req.session.user === 'none'){
-        rol = 'lectura';
-    } else if (req.session.user === 'admin') {
-        rol = 'escritura';
-    }
-    return rol;
-}
-
 module.exports = {
-    checkRol
+    checkRol: (req) => {
+        let rol;
+        if (!req.session.user){
+            req.session.user = 'none';
+            rol = 'lectura';
+        } else if (req.session.user === 'none'){
+            rol = 'lectura';
+        } else if (req.session.user === 'admin') {
+            rol = 'escritura';
+        }
+        return rol;
+    }
 }
