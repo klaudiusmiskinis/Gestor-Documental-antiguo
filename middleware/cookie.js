071d2cbf6737b8756e4d3bd5a35891783a2120dc
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-28 18:29:52 +0100

    Cambiando export

--- middleware/cookie.js
+++ middleware/cookie.js
@@ -1,13 +1,11 @@
-function listarCookies (req) {
-    let list = {},
-    rc = req;
-    rc && rc.split(';').forEach(function( cookie ) {
-        let parts = cookie.split('=');
-        list[parts.shift().trim()] = decodeURI(parts.join('='));
-    });
-    return list;
-}
-
 module.exports = {
-    listarCookies
+    listarCookies: (req) => {
+        let list = {},
+        rc = req;
+        rc && rc.split(';').forEach(function( cookie ) {
+            let parts = cookie.split('=');
+            list[parts.shift().trim()] = decodeURI(parts.join('='));
+        });
+        return list;
+    }
 }
