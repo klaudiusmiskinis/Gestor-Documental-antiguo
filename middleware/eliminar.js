7b39cfb062ed4fa83da9918524ab73a299a692f7
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-29 13:45:43 +0100

    Cambios insignificativos

--- middleware/eliminar.js
+++ middleware/eliminar.js
@@ -1,23 +1,22 @@
 const fs = require('fs');
 const { listarCookies } = require('../middleware/cookie');
 async function run(rutaRaiz, req, res){
-    let rutaArchivo = '';
+    let rutaArchivo;
     let lista = listarCookies(req.headers.cookie);
     let position = lista.position;
 
     if (position == '%2F') {
-        rutaArchivo = rutaRaiz + req.body.archivo;
+        rutaArchivo = rutaRaiz + req.body.eliminarArchivoOculto;
     } else {
         position = position.split('%2F').join('/');
         rutaArchivo = rutaRaiz + position + '/' + req.body.archivo;
     }
-
     try {
         await fs.unlinkSync(rutaArchivo);
     } catch(e) {
         return e;
     }
-    console.log('Eliminando:', req.body.archivo);
+    console.log('Eliminando:', req.body.eliminarArchivoOculto);
 }
 
 module.exports = {
