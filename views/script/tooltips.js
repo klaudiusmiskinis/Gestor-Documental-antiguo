cce622a1376d67ecec75bd5900b169633d7e3991
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-09 12:16:30 +0100

    Eliminando tippy

--- views/script/tooltips.js
+++ views/script/tooltips.js
@@ -3,13 +3,6 @@ tippy('.desplegable', {
     animation: 'fade',
 });
 
-
-tippy('#container-subir-campo', {
-    content: "Haz click para subir un archivo",
-    placement: 'bottom',
-    animation: 'fade',
-});
-
 $(document).ready(function() {
     let rutas = document.querySelectorAll('.rutas');
     rutas.forEach(ruta => {
