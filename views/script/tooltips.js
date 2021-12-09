9a3e681f24ecaff93d22adbc946ae2039addabf7
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-09 12:35:35 +0100

    Eliminando title y cambiando tooltip

--- views/script/tooltips.js
+++ views/script/tooltips.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     let rutas = document.querySelectorAll('.rutas');
     rutas.forEach(ruta => {
         tippy(ruta, {
-            content: ruta.title,
+            content: ruta.innerHTML,
             placement: 'right',
             animation: 'fade',
         })
