a934bb2fb6f32b23cfa7b103e55ea2d62be34461
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-09 13:06:22 +0100

    Añadiendo tippy a los botones del navbar

--- views/script/tooltips.js
+++ views/script/tooltips.js
@@ -3,6 +3,36 @@ tippy('.desplegable', {
     animation: 'fade',
 });
 
+tippy('#nav-volver', {
+    content: "Volver",
+    animation: 'fade',
+});
+
+tippy('#nav-registros', {
+    content: "Registros",
+    animation: 'fade',
+});
+
+tippy('#nav-home', {
+    content: "Volver al Home",
+    animation: 'fade',
+});
+
+tippy('#nav-version', {
+    content: "Alternar ultima versión",
+    animation: 'fade',
+});
+
+tippy('#nav-logout', {
+    content: "Cerrar sesión",
+    animation: 'fade',
+});
+
+tippy('#nav-login', {
+    content: "Iniciar sesión",
+    animation: 'fade',
+});
+
 $(document).ready(function() {
     let rutas = document.querySelectorAll('.rutas');
     rutas.forEach(ruta => {
