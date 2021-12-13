fb67debe1684d66973843d1e5c61b2bbec7fb69a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-13 10:27:45 +0100

    Añadiendo funcionamiento y init de la libreria dataTable

--- views/script/table.js
+++ views/script/table.js
@@ -1,32 +1,6 @@
-
-function archivos() {
-    try {
-        return new Promise(function (resolve, reject) {
-            $.get('/archivos').done(resolve).fail(reject);
-        });
-    } catch (e) {
-        return e;
-    }
-}
-let data;
-var jqxhr = $.get( "/archivos")
-    .done(function(datos) {
-        data = datos;
-    })
-    .fail(function(err) {
-      return err;
-    })
-    .always(function() {
-      return true;
-    });
-
-new gridjs.Grid({
-    columns: ["Name", "Email", "Phone Number"],
-    data: [
-      ["John", "john@example.com", "(353) 01 222 3333"],
-      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-      ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-      ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-      ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
-    ]
-  }).render(document.getElementById("tabla-registros"));
+$(document).ready(function() {
+  $('table.data').DataTable();
+  $('td').each(element => {
+      console.log(element);
+  })
+});
