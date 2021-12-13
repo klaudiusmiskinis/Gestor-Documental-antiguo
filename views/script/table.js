145090c026f04ee55ababe896aba746b0eacf141
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-13 14:01:50 +0100

    Cambiando funcionamiento de dataTable

--- views/script/table.js
+++ views/script/table.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
-  $('table.data').DataTable();
-  $('td').each(element => {
-      console.log(element);
-  })
+  const table = $('#registros').DataTable({  
+    pagingType: "numbers",
+  });
+  $('input[type="search"]').addClass('form-control mb-2');
+  $('select').addClass('form-control mb-2');
+  $('.paginate_button').addClass('btn btn-color mx-1');
+  $('.paginate_button').removeClass('paginate_button');
+  $('.current').removeClass('btn-color');
+  $('.current').addClass('border border-color border-1');
+  $('#registros_wrapper').on('DOMSubtreeModified', function(){
+    $('input[type="search"]').addClass('form-control');
+    $('select').addClass('form-control');
+    $('.paginate_button').addClass('btn btn-color mx-1');
+    $('.paginate_button').removeClass('paginate_button');
+    $('.current').removeClass('btn-color');
+    $('.current').addClass('border border-color border-1');
+  });
+  
+  const querystring = window.location.search
+  const urlparams = new URLSearchParams(querystring);
+  if (urlparams && querystring) {
+    window.history.pushState({}, document.title, "/registros");
+    table.search(urlparams.get('archivo')).draw();
+  }
 });
