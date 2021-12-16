671327faaa32d64989fbd4a91cff82402f68b3ce
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-16 13:47:31 +0100

    Cambiando funcionamiento de update

--- middleware/subir.js
+++ middleware/subir.js
@@ -4,17 +4,20 @@ const { listarCookies } = require('../middleware/cookie');
 
 async function run(rutaRaiz, req, res) {
     let rutaArchivo = '';
-    console.log(req.body);
     let lista = listarCookies(req.headers.cookie);
     let position = lista.position;
+    position = position.split('%2F').join('/');
+    if (position != '/') {
+        position = position + '/';
+    }
     let archivo = req.files.archivoContenidoOculto;
     let nombre = req.body.archivoOculto;
     let nombreArchivoSimple = req.body.nombreArchivoSimple;
-    let archivoEncontrado = await mysql.findArchivoByNombre(nombreArchivoSimple);
+    let archivoEncontrado = await mysql.findArchivoByNombre(nombreArchivoSimple, position);
     archivoEncontrado = archivoEncontrado[archivoEncontrado.length - 1]
     archivo.name = nombre;
     if (archivoEncontrado) {
-        await mysql.updateArchivoById(archivoEncontrado.idArchivo, req.body.versionOculto);
+        await mysql.updateArchivoByRuta(archivoEncontrado.idArchivo, req.body.versionOculto, position);
         await mysql.insertVersion(archivoEncontrado.idArchivo, req.body.versionOculto, req.body.dni, req.body.motivo)
     }
     if (req.files) {
