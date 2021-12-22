f482bebe1ac156d3f324fd9b194750baa2d9086b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-22 09:44:59 +0100

    Cambiando funcion document ready para no uitlizarla 2 veces

--- views/script/tooltips.js
+++ views/script/tooltips.js
@@ -33,16 +33,4 @@ tippy('#nav-login', {
     animation: 'fade',
 });
 
-$(document).ready(function() {
-    let rutas = document.querySelectorAll('.rutas');
-    rutas.forEach(ruta => {
-        tippy(ruta, {
-            content: ruta.innerHTML,
-            placement: 'right',
-            animation: 'fade',
-        })
-    })
-    
-})
-
 
