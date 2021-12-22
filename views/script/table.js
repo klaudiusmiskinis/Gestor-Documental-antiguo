2ebc267bf337171e3414260a3c1be8363c82e41a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-22 10:39:35 +0100

    Añadiendo nueva tabla con estilo y funcionamiento de dataTable, clases y eventListener

--- views/script/table.js
+++ views/script/table.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  const table = $('#registros').DataTable({  
+  
+  const tableArchivos = $('#registros').DataTable({  
     pagingType: "numbers",
   });
+  const tableVersiones = $('#versiones').DataTable({  
+    pagingType: "numbers",
+  });
+
   $('input[type="search"]').addClass('form-control mb-2');
   $('select').addClass('form-control mb-2');
   $('.paginate_button').addClass('btn btn-color mx-1');
@@ -9,18 +14,26 @@ $(document).ready(function() {
   $('.current').removeClass('btn-color');
   $('.current').addClass('border border-color border-1');
   $('#registros_wrapper').on('DOMSubtreeModified', function(){
+    dataTableStyle();
+  });
+  $('#archivos_wrapper').on('DOMSubtreeModified', function(){
+    dataTableStyle();
+  });
+
+  function dataTableStyle() {
     $('input[type="search"]').addClass('form-control');
     $('select').addClass('form-control');
     $('.paginate_button').addClass('btn btn-color mx-1');
     $('.paginate_button').removeClass('paginate_button');
     $('.current').removeClass('btn-color');
     $('.current').addClass('border border-color border-1');
-  });
+  }
   
   const querystring = window.location.search
   const urlparams = new URLSearchParams(querystring);
   if (urlparams && querystring) {
     window.history.pushState({}, document.title, "/registros");
-    table.search(urlparams.get('archivo')).draw();
+    tableVersiones.search(urlparams.get('archivo')).draw();
+    tableArchivos.search(urlparams.get('archivo')).draw();
   }
 });
